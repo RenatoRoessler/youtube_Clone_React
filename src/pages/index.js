882a8c3a070bb2886ad9e9316ba48360ac98ac4a
Renato Roessler <renatoroessler@gmail.com>
2021-05-16 21:03:46 -0300

    layout componets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
+import { Button } from "@material-ui/core"
 import React from "react"
+
+import Layout from '../components/Layout'
+
 
 export default function Home() {
   return (
-    <div>
-        clone youtube com next js
-    </div>
+    <Layout title="Youtube">
+      <div>
+        Clone youtube com Next.js + Material UI
+            <Button variant="outlined" color="secondary" >
+          Primary
+            </Button>
+      </div>
+    </Layout>
   )
 }
--- src/pages/theme/index.js
+++ src/pages/theme/index.js
@@ -5,23 +5,23 @@ import { colors } from "@material-ui/core"
 
 // Create a theme instance.
 const theme = createMuiTheme({
-   palette: {
-       background : {
-           default: colors.common.white,
-           dark: '#f4f6f8',
-           paper: colors.common.white
-       },
+  palette: {
+    background: {
+      default: colors.common.white,
+      dark: '#f4f6f8',
+      paper: colors.common.white
+    },
     primary: {
       main: '#f44336',
     },
     secondary: {
-      main: '#3WA6FF',
+      main: '#3EA6FF',
     },
     text: {
-        primary: colors.blueGrey[900],
-        secondary: colors.blueGrey[600],
+      primary: colors.blueGrey[900],
+      secondary: colors.blueGrey[600],
     }
-  }, 
+  },
 });
 
 export default theme;
