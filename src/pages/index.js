3d9aa636e992942018c042b532ea74c356fb4971
renato <renatoroessler@gmail.com>
2021-05-16 22:34:07 -0300

    componete videos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,70 @@
-import { Button } from "@material-ui/core"
+import { Box, Button, Grid } from "@material-ui/core"
 import React from "react"
 
-import Layout from '../components/Layout'
+import Layout from 'src/components/Layout'
+import VideoCard from 'src/components/VideoCard'
 
 
-export default function Home() {
+function Home({ data }) {
   return (
-    <Layout title="Youtube">
-      <div>
-        Clone youtube com Next.js + Material UI
-            <Button variant="outlined" color="secondary" >
-          Primary
-            </Button>
-      </div>
+    <Layout title="YouTube">
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.id} item xl={3} lg={3} md={4} xs={12}>
+              <VideoCard item={item}></VideoCard>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   )
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER [PARTE 01',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarURL',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updateAt: new Date()
+    },
+    {
+      id: 2,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER [PARTE 02]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarURL',
+      views: 10,
+      thumb: '/thumbs/next02.png',
+      videoUrl: 'url',
+      updateAt: new Date()
+    },
+    {
+      id: 3,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER [PARTE 02',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarURL',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updateAt: new Date()
+    },
+  ]
+  return {
+    
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    }
+  }
+}
+
+
+
+export default Home
